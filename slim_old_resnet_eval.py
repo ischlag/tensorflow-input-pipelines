1a75b768c12060fb9b4372f3e0cf7f4b7b8c466d
ischlag <imanol.schlag@gmail.com>
2016-12-13 15:34:18 +0100

    added paper version scripts

---
+++ slim_old_resnet_eval.py
@@ -0,0 +1,140 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import os
+import time
+import math
+from datetime import datetime
+
+from datasets.cifar10 import cifar10_data
+from datasets.cifar100 import cifar100_data
+
+from libs import custom_ops
+from nets import bn_conv
+
+log_dir = "logs/cifar10/wrn_2/"
+eval_dir = log_dir
+batch_size = 128
+num_classes = 10
+epoch_size = 10000.0
+num_iter = int(math.ceil(epoch_size/batch_size))
+load_latest_checkpoint = False
+
+eval_interval_secs = 3
+run_once = False
+
+tf.logging.set_verbosity(tf.logging.INFO)
+sess = tf.Session()
+
+## Data
+with tf.device('/cpu:0'):
+  d = cifar10_data(batch_size=batch_size, sess=sess)
+  image_batch_tensor, target_batch_tensor = d.build_test_data_tensor(shuffle=False, augmentation=False)
+
+## Model
+#logits = bn_conv.inference(image_batch_tensor, num_classes=num_classes, is_training=True)
+#from tensorflow.contrib.slim.nets import resnet_v2
+#with slim.arg_scope(custom_ops.resnet_arg_scope(is_training=True)):
+#  net, end_points = resnet_v2.resnet_v2_101(image_batch_tensor,
+#                                              num_classes=num_classes,
+#                                              global_pool=True)# reduce output to rank 2 (not working)
+#logits = tf.reduce_mean(net, [1, 2], name='pool5', keep_dims=False)
+import nets.resnet_old_reference
+hps = nets.resnet_old_reference.HParams(batch_size=batch_size,
+                          num_classes=num_classes,
+                          min_lrn_rate=None,
+                          lrn_rate=None,
+                          num_residual_units=10,
+                          use_bottleneck=False,
+                          weight_decay_rate=0.0002,
+                          relu_leakiness=0.1,
+                          optimizer='mom')
+model = nets.resnet_old_reference.ResNet(hps, image_batch_tensor, target_batch_tensor, 'eval')
+model.build_graph()
+
+## Losses and Accuracies
+
+## Optimizer
+
+## Summaries
+
+## Initialization
+saver = tf.train.Saver(max_to_keep=10000000)
+summary_writer = tf.train.SummaryWriter(eval_dir)
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+sess.run(tf.global_variables_initializer())
+
+
+def _eval_model_checkpoint(model_checkpoint_path):
+  if model_checkpoint_path:
+    tf.logging.info("Restoring from checkpoint %s" % model_checkpoint_path)
+    saver.restore(sess, model_checkpoint_path)
+  else:
+    tf.logging.error("Couldn't find checkpoint to restore from. Exiting.")
+    return
+
+  # Counts the number of correct predictions.
+  total_loss = 0.0
+  total_sample_count = num_iter * batch_size
+  step = 0
+  global_step = model_checkpoint_path.split('/')[-1].split('-')[-1]
+
+  tf.logging.info('%s: starting evaluation.' % (datetime.now()))
+  start_time = time.time()
+  correct_prediction, total_prediction = 0, 0
+  while step < num_iter and not coord.should_stop():
+    loss_value, predictions, truth = sess.run([model.cost, model.predictions, model.labels])
+
+    total_loss += np.sum(loss_value)
+    truth = np.argmax(truth, axis=1)
+    predictions = np.argmax(predictions, axis=1)
+    correct_prediction += np.sum(truth == predictions)
+    total_prediction += predictions.shape[0]
+
+    step += 1
+    if step % 200 == 0:
+      duration = time.time() - start_time
+      sec_per_batch = duration / 20.0
+      examples_per_sec = batch_size / sec_per_batch
+      tf.logging.info('[%d batches out of %d] (%.1f examples/sec; %.3f'
+            'sec/batch)' % (step, num_iter, examples_per_sec, sec_per_batch))
+      start_time = time.time()
+
+  # compute test set accuracy
+  accuracy = correct_prediction * 100.0 / total_prediction
+  avg_loss = total_loss / total_sample_count
+
+  tf.logging.info('%s: top_1_acc: %6.3f%%, avg_loss: %.7f [%d examples]' %
+        (global_step, accuracy, avg_loss, total_sample_count))
+
+  accuracy_sum = tf.Summary(value=[tf.Summary.Value(tag="test/accuracy", simple_value=accuracy)])
+  avg_loss_summary = tf.Summary(value=[tf.Summary.Value(tag="test/avg_loss", simple_value=avg_loss)])
+  summary_writer.add_summary(accuracy_sum, global_step)
+  summary_writer.add_summary(avg_loss_summary, global_step)
+  summary_writer.flush()
+
+## Eval
+if run_once:
+  ckpt = tf.train.get_checkpoint_state(log_dir)
+  if ckpt and ckpt.model_checkpoint_path:
+    _eval_model_checkpoint(ckpt.model_checkpoint_path)
+  else:
+    tf.logging.error('No checkpoint file found')
+    exit()
+
+else:
+  done = []
+  while True:
+    tf.logging.info("checking for new models in %s ... " % log_dir)
+    ckpt = tf.train.get_checkpoint_state(log_dir)
+    if ckpt and ckpt.model_checkpoint_path:
+      for path in ckpt.all_model_checkpoint_paths:
+        if not path in done:
+          done.append(path)
+          _eval_model_checkpoint(path)
+    else:
+      tf.logging.error('No checkpoint file found')
+    time.sleep(eval_interval_secs)
+
+print("done!")
---
+++ slim_old_resnet_train.py
@@ -0,0 +1,136 @@
+import tensorflow as tf
+import tensorflow.contrib.slim as slim
+import numpy as np
+import os
+import time
+from collections import deque
+
+from datasets.cifar10 import cifar10_data
+from datasets.cifar100 import cifar100_data
+
+from libs import components
+from libs import custom_ops
+from nets import bn_conv
+
+tf.logging.set_verbosity(tf.logging.INFO)
+
+log_dir = "logs/cifar10/wrn_paper_version/"
+ckpt_dir = log_dir # "logs/cifar10/wrn_1/"
+batch_size = 64
+num_classes = 10
+epoch_in_steps = int(50000.0/batch_size)
+max_step = epoch_in_steps * 15
+load_latest_checkpoint = False
+step = 0
+lrn_rate = 0.1
+
+sess = tf.Session()
+
+## Data
+with tf.device('/cpu:0'):
+  d = cifar10_data(batch_size=batch_size, sess=sess)
+  image_batch_tensor, target_batch_tensor = d.build_train_data_tensor(shuffle=True, augmentation=True)
+
+## Model
+#logits = bn_conv.inference(image_batch_tensor, num_classes=num_classes, is_training=True)
+#logits = highway_test.inference(image_batch_tensor, num_classes=num_classes, is_training=True)
+#from tensorflow.contrib.slim.nets import resnet_v2
+#with slim.arg_scope(custom_ops.resnet_arg_scope(is_training=True)):
+#  net, end_points = resnet_v2.resnet_v2_101(image_batch_tensor,
+#                                                num_classes=num_classes,
+#                                              global_pool=True)# reduce output to rank 2 (not working)
+#logits = tf.reduce_mean(net, [1, 2], name='pool5', keep_dims=False)
+
+#import nets.resnet
+import nets.resnet_old_reference
+hps = nets.resnet_old_reference.HParams(batch_size=batch_size,
+                          num_classes=num_classes,
+                          min_lrn_rate=0.0001,
+                          lrn_rate=0.1,
+                          num_residual_units=9,
+                          use_bottleneck=False,
+                          weight_decay_rate=0.0002,
+                          relu_leakiness=0.1,
+                          optimizer='mom')
+model = nets.resnet_old_reference.ResNet(hps, image_batch_tensor, target_batch_tensor, 'train')
+model.build_graph()
+
+## Losses and Accuracies
+avg_loss_queue = deque(epoch_in_steps * [0])
+avg_top1_queue = deque(epoch_in_steps * [0])
+
+## Optimizer
+
+## Summaries
+summary_op = model.summaries
+
+## Initialization
+saver = tf.train.Saver(max_to_keep=10000000)
+summary_writer = tf.train.SummaryWriter(log_dir, graph=sess.graph)
+coord = tf.train.Coordinator()
+threads = tf.train.start_queue_runners(coord=coord, sess=sess)
+sess.run(tf.global_variables_initializer())
+
+## Load Pretrained
+if load_latest_checkpoint:
+  checkpoint = tf.train.latest_checkpoint(ckpt_dir)
+  if checkpoint:
+    tf.logging.info("Restoring from checkpoint %s" % checkpoint)
+    saver.restore(sess, checkpoint)
+    step = sess.run(model.global_step)
+  else:
+    tf.logging.error("Couldn't find checkpoint to restore from. Exiting.")
+    exit()
+
+## Train
+tf.logging.info('start training ...')
+total_start_time = time.time()
+while not coord.should_stop():
+  start_time = time.time()
+  correct_prediction, total_prediction = 0, 0
+  (_, summaries, loss, train_step, predictions, truth) = sess.run(
+    [model.train_op, summary_op, model.cost, model.global_step, model.predictions, model.labels],
+    feed_dict={model.lrn_rate: lrn_rate})
+
+  if train_step < 40000: # 15000: # 40000
+    lrn_rate = 0.1
+  elif train_step < 60000: #30000: # 60000
+    lrn_rate = 0.01
+  elif train_step < 80000: # # 80000
+    lrn_rate = 0.001
+  else:
+    lrn_rate = 0.0001
+
+  duration = time.time() - start_time
+  truth = np.argmax(truth, axis=1)
+  predictions = np.argmax(predictions, axis=1)
+  accuracy = np.sum(truth == predictions) * 100 / batch_size
+  avg_accuracy = components.push_into_queue(accuracy, avg_top1_queue, "train/avg_accuracy", train_step, summary_writer)
+  avg_loss = components.push_into_queue(loss, avg_loss_queue, "train/avg_loss", train_step, summary_writer)
+
+  if step % 100 == 0:
+    total_duration = (time.time() - total_start_time) / 60.0
+    examples_per_sec = batch_size / float(duration)
+    accuracy_sum = tf.Summary(value=[tf.Summary.Value(tag="train/accuracy", simple_value=accuracy)])
+
+    if step == 500:
+      format_str = ('%4.2fmin, step %4.d, lr: %.4f, loss: %4.3f, top-1: %5.2f%% (%.1f examples/sec; %.3f sec/batch)')
+      tf.logging.info(format_str % (total_duration, step, lrn_rate, loss, accuracy, examples_per_sec, duration))
+    else:
+      format_str = ('%4.2fmin, step %4.d, lr: %.4f, loss: %4.3f (%4.3f), top-1: %5.2f%% (%5.2f%%)')
+      tf.logging.info(format_str % (total_duration, step, lrn_rate, loss, avg_loss, accuracy, avg_accuracy))
+
+    summary_writer.add_summary(accuracy_sum, train_step)
+    summary_writer.add_summary(summaries, train_step)
+    summary_writer.flush()
+
+  if step % 1000 == 0:
+    tf.logging.info("saving checkpoint")
+    checkpoint_path = os.path.join(log_dir, 'model.ckpt')
+    saver.save(sess, checkpoint_path, global_step=model.global_step)
+
+  step += 1
+
+coord.join(threads)
+
+print("done!")
